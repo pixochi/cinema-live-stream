efb8d795fb132e31f8d20acc245f5c0c12cf5694
pixochi <kozak.jakub55@gmail.com>
2021-09-12 09:23:48 +0200

    Create a form for new live stream

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useCallback } from "react";
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [url, setUrl] = useState("");
+  const [startTime, setStartTime] = useState("");
+
+  const handleSubmit = useCallback(
+    e => {
+      e.preventDefault();
+
+      fetch("http://localhost:3001/v1/liveStream", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ title, url, startTime })
+      }).then(() => {
+        setTitle("");
+        setUrl("");
+        setStartTime("");
+      });
+    },
+    [title, url, startTime]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <p>Title</p>
+      <input
+        type="text"
+        value={title}
+        onChange={e => setTitle(e.target.value)}
+      />
+      <p>YouTube url</p>
+      <input type="text" value={url} onChange={e => setUrl(e.target.value)} />
+      <p>Start time</p>
+      <input
+        type="datetime-local"
+        value={startTime}
+        onChange={e => setStartTime(e.target.value)}
+      />
+      <input type="submit" />
+    </form>
   );
 }
 
