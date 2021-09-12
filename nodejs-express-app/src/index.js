06306bd063c01a0a463c1e8522cdff7f9f61fbdb
pixochi <kozak.jakub55@gmail.com>
2021-09-12 08:44:32 +0200

    Use port 3001 for express server

--- nodejs-express-app/src/index.js
+++ nodejs-express-app/src/index.js
@@ -6,8 +6,8 @@ const logger = require('./config/logger');
 let server;
 mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
   logger.info('Connected to MongoDB');
-  server = app.listen(config.port, () => {
-    logger.info(`Listening to port ${config.port}`);
+  server = app.listen(3001, () => {
+    logger.info(`Listening to port ${3001}`);
   });
 });
 
